de6e4f19d33990ae89105f7e790fd58c2afdcaa2
Ajay Yadav <ajay.yadav.210698@gmail.com>
2024-06-07 17:40:47 +0530

    firstCommit

---
+++ PresentationPractice/src/test/java/Presentation/SampleTest.java
@@ -0,0 +1,12 @@
+package Presentation;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+    @Test
+	public void Sample1() {
+    	System.out.println("Presentation");
+		
+	}
+
+}
